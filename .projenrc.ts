3fdd9a05e758a0d178c20e8d79c12e13161a90a4
Maksim Yersh <m.yersh@admitad.com>
2023-07-23 20:54:23 +0300

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,12 @@
+import { NxMonorepoProject } from "@aws-prototyping-sdk/nx-monorepo";
+const project = new NxMonorepoProject({
+  defaultReleaseBranch: "main",
+  devDeps: ["@aws-prototyping-sdk/nx-monorepo"],
+  name: "GrowthHub",
+  projenrcTs: true,
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
