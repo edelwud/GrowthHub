95b1128d96cfaf0c4ed08a661751c2114ce2714a
Maksim Yersh <m.yersh@admitad.com>
2023-07-25 21:56:11 +0300

    feat: add initial skill microservice

---
+++ microservices/skills/release.config.js
@@ -0,0 +1,37 @@
+const appName = 'skills';
+const appPath = 'microservices/skills';
+const artifactName = appName;
+module.exports = {
+  name: appName,
+  pkgRoot: 'lib/',
+  tagFormat: artifactName + '-v${version}',
+  commitPaths: ['*'],
+  assets: ['README.md', 'CHANGELOG.md'],
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: `CHANGELOG.md`,
+      },
+    ],
+    [
+      '@semantic-release-plus/docker',
+      {
+        name: `ghcr.io/edelwud/${appName}`,
+        skipLogin: 'true'
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md'],
+        message:
+          `chore(release): ${artifactName}` +
+          '-v${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
